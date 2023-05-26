eaf44ce4f3f85239b4293e94723d45f6cb21cf67
puugz <matej1.svetina@gmail.com>
2023-05-26 17:09:55 +0200

    chore: change messages color from dark blue to blue

--- src/main/java/me/puugz/meetup/config/MessagesConfig.java
+++ src/main/java/me/puugz/meetup/config/MessagesConfig.java
@@ -15,7 +15,7 @@ public class MessagesConfig {
     public final String navigationMenuTitle = ChatColor.GRAY + "Navigation";
 
     @Coloured
-    public final String navigationItem = ChatColor.DARK_BLUE + "Navigator";
+    public final String navigationItem = ChatColor.BLUE + "Navigator";
 
     @Coloured
     public final String nowSpectating = ChatColor.GRAY + "You are now a spectator of this game.";
@@ -24,13 +24,13 @@ public class MessagesConfig {
     public final String teleportInsideBorder = ChatColor.RED + "You have been teleported inside the border.";
 
     @Coloured
-    public final String playerJoined = ChatColor.DARK_BLUE + "{player} " + ChatColor.GRAY + "has joined the game.";
+    public final String playerJoined = ChatColor.BLUE + "{player} " + ChatColor.GRAY + "has joined the game.";
 
     @Coloured
-    public final String playerQuit = ChatColor.DARK_BLUE + "{player} " + ChatColor.GRAY + "has left the game.";
+    public final String playerQuit = ChatColor.BLUE + "{player} " + ChatColor.GRAY + "has left the game.";
 
     @Coloured
-    public final String playerDisqualified = ChatColor.DARK_BLUE + "{player} " + ChatColor.RED + "has left and been disqualified!";
+    public final String playerDisqualified = ChatColor.BLUE + "{player} " + ChatColor.RED + "has left and been disqualified!";
 
     @Coloured
     public final String startingCancelled = ChatColor.RED + "The game has been cancelled due to a player leaving.";
@@ -39,32 +39,32 @@ public class MessagesConfig {
     public final String minimumRequiredPlayers = ChatColor.GRAY + "Minimum of {min} players is required for the game to start.";
 
     @Coloured
-    public final String borderShrink = ChatColor.GRAY + "The border will shrink to " + ChatColor.DARK_BLUE + "{size} " + ChatColor.GRAY + "in " + ChatColor.DARK_BLUE + "{time}" + ChatColor.GRAY + ".";
+    public final String borderShrink = ChatColor.GRAY + "The border will shrink to " + ChatColor.BLUE + "{size} " + ChatColor.GRAY + "in " + ChatColor.BLUE + "{time}" + ChatColor.GRAY + ".";
 
     @Coloured
-    public final String borderShrunk = ChatColor.GRAY + "The border has shrunk to " + ChatColor.DARK_BLUE + "{size}" + ChatColor.GRAY + ".";
+    public final String borderShrunk = ChatColor.GRAY + "The border has shrunk to " + ChatColor.BLUE + "{size}" + ChatColor.GRAY + ".";
 
     @Coloured
-    public final String killedBy = ChatColor.GRAY + "You have been killed by " + ChatColor.DARK_BLUE + "{killer}" + ChatColor.GRAY + ".";
+    public final String killedBy = ChatColor.GRAY + "You have been killed by " + ChatColor.BLUE + "{killer}" + ChatColor.GRAY + ".";
 
     @Coloured
-    public final String killedPlayer = ChatColor.GRAY + "You have killed " + ChatColor.DARK_BLUE + "{victim}" + ChatColor.GRAY + ".";
+    public final String killedPlayer = ChatColor.GRAY + "You have killed " + ChatColor.BLUE + "{victim}" + ChatColor.GRAY + ".";
 
     @Coloured
-    public final String slainByKiller = ChatColor.DARK_BLUE + "{victim} " + ChatColor.GRAY + "was slain by " + ChatColor.DARK_BLUE + "{killer}" + ChatColor.GRAY + ".";
+    public final String slainByKiller = ChatColor.BLUE + "{victim} " + ChatColor.GRAY + "was slain by " + ChatColor.BLUE + "{killer}" + ChatColor.GRAY + ".";
 
     @Coloured
-    public final String died = ChatColor.DARK_BLUE + "{player} " + ChatColor.GRAY + "has died.";
+    public final String died = ChatColor.BLUE + "{player} " + ChatColor.GRAY + "has died.";
 
     @Coloured
-    public final String serverRestarting = ChatColor.GRAY + "The server will be restarting in " + ChatColor.DARK_BLUE + "{time}" + ChatColor.GRAY + ".";
+    public final String serverRestarting = ChatColor.GRAY + "The server will be restarting in " + ChatColor.BLUE + "{time}" + ChatColor.GRAY + ".";
 
     @Coloured
     public final String serverRestart = ChatColor.RED + "Server restarting...";
 
     // TODO: Make the whole win message configurable
     @Coloured
-    public final String win = ChatColor.DARK_BLUE + "{winner} " + ChatColor.GREEN + "wins!";
+    public final String win = ChatColor.BLUE + "{winner} " + ChatColor.GREEN + "wins!";
 
     @Coloured
     public final String spectatorPrefix = ChatColor.GRAY + "[Spectator] " + ChatColor.RESET + "{format}";
@@ -79,7 +79,7 @@ public class MessagesConfig {
     public final String forcestarted = ChatColor.GREEN + "Forced the countdown to 5 seconds!";
 
     @Coloured
-    public final String gameStarting = ChatColor.GRAY + "The game will begin in " + ChatColor.DARK_BLUE + "{time}" + ChatColor.GRAY + ".";
+    public final String gameStarting = ChatColor.GRAY + "The game will begin in " + ChatColor.BLUE + "{time}" + ChatColor.GRAY + ".";
 
     @Coloured
     public final String gameStarted = ChatColor.GRAY + "The game has started!";
@@ -94,7 +94,10 @@ public class MessagesConfig {
     public final String cantSetSpawnHere = ChatColor.RED + "You can't set the spawn in this world.";
 
     @Coloured
-    public final String teleportedTo = ChatColor.GRAY + "You have teleported to " + ChatColor.DARK_BLUE + "{player}" + ChatColor.GRAY + ".";
+    public final String timeBombExplosion = ChatColor.BLUE + "{player}" + ChatColor.GRAY + "'s corspe has exploded!";
+
+    @Coloured
+    public final String teleportedTo = ChatColor.GRAY + "You have teleported to " + ChatColor.BLUE + "{player}" + ChatColor.GRAY + ".";
 
     @Coloured
     public final String clickToTeleport = ChatColor.GREEN + "Click to teleport.";
--- src/main/java/me/puugz/meetup/config/ScoreboardsConfig.java
+++ src/main/java/me/puugz/meetup/config/ScoreboardsConfig.java
@@ -13,14 +13,14 @@ import java.util.List;
 public class ScoreboardsConfig {
 
     @Coloured
-    public final String title = ChatColor.DARK_BLUE + ChatColor.BOLD.toString() + "UHC Meetup";
+    public final String title = ChatColor.BLUE + ChatColor.BOLD.toString() + "UHC Meetup";
 
     @Coloured
     public final List<String> waiting = Arrays.asList(
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------",
             "Waiting for players",
             "",
-            ChatColor.DARK_BLUE + "meetup.lol",
+            ChatColor.BLUE + "meetup.lol",
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------"
     );
 
@@ -28,34 +28,34 @@ public class ScoreboardsConfig {
     public final List<String> starting = Arrays.asList(
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------",
             "The game will start in:",
-            ChatColor.DARK_BLUE + "{time}",
+            ChatColor.BLUE + "{time}",
             "",
-            ChatColor.DARK_BLUE + "meetup.lol",
+            ChatColor.BLUE + "meetup.lol",
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------"
     );
 
     @Coloured
     public final List<String> playing = Arrays.asList(
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------",
-            "Border: " + ChatColor.DARK_BLUE + "{border_size} {border_format}",
-            "Players: " + ChatColor.DARK_BLUE + "{players}",
-            "Ping: " + ChatColor.DARK_BLUE + "{ping} ms",
-            "Kills: " + ChatColor.DARK_BLUE + "{kills}",
+            "Border: " + ChatColor.BLUE + "{border_size} {border_format}",
+            "Players: " + ChatColor.BLUE + "{players}",
+            "Ping: " + ChatColor.BLUE + "{ping} ms",
+            "Kills: " + ChatColor.BLUE + "{kills}",
             "",
-            ChatColor.DARK_BLUE + "meetup.lol",
+            ChatColor.BLUE + "meetup.lol",
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------"
     );
 
     @Coloured
     public final List<String> ending = Arrays.asList(
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------",
-            "Kills: " + ChatColor.DARK_BLUE + "{kills}",
-            "Winner: " + ChatColor.DARK_BLUE + "{winner}",
+            "Kills: " + ChatColor.BLUE + "{kills}",
+            "Winner: " + ChatColor.BLUE + "{winner}",
             "",
-            ChatColor.DARK_BLUE + "meetup.lol",
+            ChatColor.BLUE + "meetup.lol",
             ChatColor.GRAY + ChatColor.STRIKETHROUGH.toString() + "--------------------"
     );
 
     @Coloured
-    public final String borderFormat = ChatColor.RESET + "(" + ChatColor.DARK_BLUE + "{border_time}" + ChatColor.RESET + ")";
+    public final String borderFormat = ChatColor.RESET + "(" + ChatColor.RED + "{border_time}" + ChatColor.RESET + ")";
 }
